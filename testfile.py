18ad91911cb5610fc6c81f071a8bf32c60bd2707
ConBoe <constantin.b@gmx.at>
2023-07-14 13:52:13 +0200

    testfile playaround

--- testfile.py
+++ testfile.py
@@ -1,26 +1,10 @@
 import numpy as np
 from collections import defaultdict
 
-
-def normalizedProps(size):
-    randomArray=np.random.rand(size)
-    randomArray/= randomArray.sum()
-    return (lambda: randomArray)
+a= 1
+mytup= (a,a)
 
 
 
-
-somesize=3
-Q = defaultdict(normalizedProps(3))
-
-
-
-
-
-
-mylist=[1,2,3]
-
-for i in range(100):
-    tmp=np.random.choice(mylist, p=Q[i])
-    print(Q[i])
-    print(tmp)
+print(tuple(mytup))
+print(tuple(a))
